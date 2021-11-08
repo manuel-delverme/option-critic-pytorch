5f8d4e1a8f98f43cfcbc79aafe34ad99e60c4854
manuel <manuel.delverme@gmail.com>
2021-11-08 20:55:49 +0100

    barebones setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='option_critic',
+    version='0.0.1',
+    description='option_critic',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+)
