0b20a5a6f58acf8e52e15e3aa057dc91a0153946
manuel <manuel.delverme@gmail.com>
2021-11-08 17:58:55 +0100

    temp commit

--- option_critic.py
+++ option_critic.py
@@ -1,93 +1,10 @@
 from math import exp
+import torch
 
 import numpy as np
-import torch
-import torch.nn
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical, Bernoulli
-
-
-class OptionCriticConv(nn.Module):
-    def __init__(self, in_features, num_actions, num_options, temperature=1.0, eps_start=1.0, eps_min=0.1, eps_decay=int(1e6), eps_test=0.05, device='cpu', testing=False):
-
-        super(OptionCriticConv, self).__init__()
-
-        self.in_channels = in_features
-        self.num_actions = num_actions
-        self.num_options = num_options
-        self.magic_number = 7 * 7 * 64
-        self.device = device
-        self.testing = testing
-
-        self.temperature = temperature
-        self.eps_min = eps_min
-        self.eps_start = eps_start
-        self.eps_decay = eps_decay
-        self.eps_test = eps_test
-        self.num_steps = 0
-
-        self.features = nn.Sequential(
-            nn.Conv2d(self.in_channels, 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU(),
-            nn.modules.Flatten(),
-            nn.Linear(self.magic_number, 512),
-            nn.ReLU()
-        )
-
-        self.Q = nn.Linear(512, num_options)  # Policy-Over-Options
-        self.terminations = nn.Linear(512, num_options)  # Option-Termination
-        self.options_W = nn.Parameter(torch.zeros(num_options, 512, num_actions))
-        self.options_b = nn.Parameter(torch.zeros(num_options, num_actions))
-
-        self.to(device)
-        self.train(not testing)
-
-    # def get_state(self, obs):
-    #     if obs.ndim < 4:
-    #         obs = obs.unsqueeze(0)
-    #     obs = obs.to(self.device)
-    #     state = self.features(obs)
-    #     return state
-
-    def predict_option_termination(self, state, current_option):
-        termination = self.terminations(state)[:, current_option].sigmoid()
-        option_termination = Bernoulli(termination).sample()
-
-        Q = self.Q(state)
-        next_option = Q.argmax(dim=-1)
-        return bool(option_termination.item()), next_option.item()
-
-    def get_terminations(self, state):
-        return self.terminations(state).sigmoid()
-
-    def get_action(self, state, option):
-        logits = state @ self.options_W[option] + self.options_b[option]
-        action_dist = (logits / self.temperature).softmax(dim=-1)
-        action_dist = Categorical(action_dist)
-
-        action = action_dist.sample()
-        logp = action_dist.log_prob(action)
-        entropy = action_dist.entropy()
-
-        return action.item(), logp, entropy
-
-    def greedy_option(self, state):
-        Q = self.Q(state)
-        return Q.argmax(dim=-1).item()
-
-    @property
-    def epsilon(self):
-        if not self.testing:
-            eps = self.eps_min + (self.eps_start - self.eps_min) * exp(-self.num_steps / self.eps_decay)
-            self.num_steps += 1
-        else:
-            eps = self.eps_test
-        return eps
 
 
 class OptionCriticFeatures(nn.Module):
@@ -119,15 +36,10 @@ class OptionCriticFeatures(nn.Module):
             nn.Linear(64, num_options),
             nn.Tanh(),
         )
-        self.options_W = nn.Parameter(torch.zeros(num_options, 64, num_actions))
-        self.options_b = nn.Parameter(torch.zeros(num_options, num_actions))
+        self.Q_options = nn.Linear(64, num_actions * num_options)
 
         self.to(device)
         self.train(not testing)
-
-    # def get_state(self, obs):
-    #     state = self.features(obs)
-    #     return state
 
     def predict_option_termination(self, state, current_option):
         termination = self.terminations(state)[:, current_option].sigmoid()
@@ -136,13 +48,11 @@ class OptionCriticFeatures(nn.Module):
         # next_option = Q.argmax(dim=-1)
         return bool(option_termination.item())  # , next_option.item()
 
-    def get_terminations(self, state):
-        raise NotImplementedError
-        return self.terminations(state).sigmoid()
-
     def option_pi(self, option, state):
-        logits = state @ self.options_W[option] + self.options_b[option]
-        action_dist = (logits / self.temperature).softmax(dim=-1)
+        logits_Q = self.Q_options(state).reshape(-1, self.num_options, self.num_actions)
+        logits_Qo = logits_Q[torch.arange(logits_Q.shape[0]), option, :].squeeze(1)
+        # logits_Qo = logits_Q.gather(1, option)
+        action_dist = (logits_Qo / self.temperature).softmax(dim=-1)
         return Categorical(action_dist)
 
     def get_action(self, state, option):
@@ -173,6 +83,34 @@ class OptionCriticFeatures(nn.Module):
         return eps
 
 
+class OptionCriticConv(OptionCriticFeatures):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.in_channels = self.in_features
+        del self.in_features
+        self.magic_number = 7 * 7 * 64
+        self.num_steps = 0
+
+        self.features = nn.Sequential(
+            nn.Conv2d(self.in_channels, 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.modules.Flatten(),
+            nn.Linear(self.magic_number, 512),
+            nn.ReLU()
+        )
+
+        self.Q = nn.Linear(512, self.num_options)  # Policy-Over-Options
+        self.terminations = nn.Linear(512, self.num_options)  # Option-Termination
+        self.Q_options = nn.Linear(512, self.num_actions * self.num_options)
+
+        self.to(self.device)
+        self.train(not self.testing)
+
+
 class OptionCriticTabular(OptionCriticFeatures):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -184,7 +122,7 @@ class OptionCriticTabular(OptionCriticFeatures):
             nn.Linear(self.in_features, self.num_options),
             nn.Tanh(),
         )
-        self.options_W = nn.Parameter(torch.zeros(self.num_options, self.in_features, self.num_actions))
+        self.Q_options = nn.Linear(self.in_features, self.num_actions * self.num_options)
         self.to(device=device)
 
 
